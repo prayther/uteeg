cce15c4b3ce95d268c56af0db0a068cdf5ae1bc7
Aaron Prayther <prayther@gmail.com>
2017-07-06 12:24:06 -0400

    cv_promote and hc_create

--- hc_create.sh
+++ hc_create.sh
@@ -5,4 +5,14 @@
 # Lifecycle
 # App
 
+# Run after creating Content Views and Lifecycles. It uses those to create the Host Collections.
+
+for CV in $(hammer --csv content-view list --organization redhat | grep CCV | awk -F"," '{print$2}' | sed 's/^[^_]*_//g');do
+  for LE in $(hammer --csv lifecycle-environment list --organization redhat | grep -v "Library" | grep -v "Name" | awk -F"," '{print $2}');do
+    hammer host-collection create --name="HC_${LE}_${CV}" --organization=redhat
+  done
+done
+
+
+
 
