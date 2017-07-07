7fb892bafcb99af1f8281dc1e303301d89708f85
Aaron Prayther <prayther@gmail.com>
2017-07-07 08:32:22 -0400

    ak_create.cfg

--- bin/ak_create.sh
+++ bin/ak_create.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+cd "${BASH_SOURCE%/*}"
+source ../etc/ak_create.cfg 
+
 # Create Activation Keys.
 # This script will run after cv create, lifecycle create, cv_promote.sh
 #hammer activation-key create \
@@ -14,8 +17,10 @@
 # 4 Layered for ceph, CFME, Satellite that would already have a rhel sub. You would have to isoliate to esxi hosts that do not have rhel subs, other wise you would use a rhel sub and a ceph sub for rhel
 
 # Add a AK for each CCV. Seems like a good idea.
-for CCV in $(hammer --csv content-view list --organization redhat | grep CCV | awk -F"," '{print$2}' | sed 's/^[^_]*_//g');do
-  for LE in $(hammer --csv lifecycle-environment list --organization redhat | awk -F"," '{print $2}' | grep -v "Library" | grep -v "Name");do
+#for CCV in $(hammer --csv content-view list --organization redhat | grep CCV | awk -F"," '{print$2}' | sed 's/^[^_]*_//g');do
+#  for LE in $(hammer --csv lifecycle-environment list --organization redhat | awk -F"," '{print $2}' | grep -v "Library" | grep -v "Name");do
+for CCV in ${CCV_var};do
+  for LE in ${LE_var};do
     hammer activation-key create --name "AK_${LE}_${CCV}" --organization=redhat --lifecycle-environment ${LE} --content-view CCV_${CCV}
     hammer activation-key add-host-collection --name "AK_${LE}_${CCV}" --organization=redhat --host-collection HC_${LE}_${CCV}
   done
