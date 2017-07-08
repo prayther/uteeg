ac5b7aba7e73b54508402bc5d754cbbd009463a7
root <root@sat.laptop.prayther>
2017-07-08 17:01:57 -0400

    install

--- bin/install-configure-satellite.sh
+++ bin/install-configure-satellite.sh
@@ -186,15 +186,15 @@ hammer settings set --name idle_timeout --value 99999999
 #    Public_Website -> Dev -> Test -> Prod
 #    App -> Dev -> Test -> UAT -> Prod -> Legacy
  
-hammer lifecycle-environment create --name='Infra_Dev' --prior='Library' --organization=redhat
-hammer lifecycle-environment create --name='Infra_Test' --prior='Infra_Dev' --organization=redhat
-hammer lifecycle-environment create --name='Infra_Prod' --prior='Infra_Test' --organization=redhat
+hammer lifecycle-environment create --name='Infra_Dev' --prior='Library' --organization="${ORG}"
+hammer lifecycle-environment create --name='Infra_Test' --prior='Infra_Dev' --organization="${ORG}"
+hammer lifecycle-environment create --name='Infra_Prod' --prior='Infra_Test' --organization="${ORG}"
  
  
-hammer lifecycle-environment create --name='App_Dev' --prior='Library' --organization=redhat
-hammer lifecycle-environment create --name='App_Test' --prior='App_Dev' --organization=redhat
-hammer lifecycle-environment create --name='App_UAT' --prior='App_Test' --organization=redhat
-hammer lifecycle-environment create --name='App_Prod' --prior='App_UAT' --organization=redhat
+hammer lifecycle-environment create --name='App_Dev' --prior='Library' --organization="${ORG}"
+hammer lifecycle-environment create --name='App_Test' --prior='App_Dev' --organization="${ORG}"
+hammer lifecycle-environment create --name='App_UAT' --prior='App_Test' --organization="${ORG}"
+hammer lifecycle-environment create --name='App_Prod' --prior='App_UAT' --organization="${ORG}"
  
 # This is How I get the ID of a repo by name
 #hammer --csv repository list --organization=redhat |  awk -F, '/^[0-9]*?,Red Hat Enterprise Linux 6 Server RPMs x86_64 6Server/ {print $1}'
