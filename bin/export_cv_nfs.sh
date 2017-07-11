e330a4231f54170fdfc1834569ce28b55eb1d4dd
root <root@fedora.prayther.laptop>
2017-07-11 19:22:37 -0400

    nfs

--- bin/export_cv_nfs.sh
+++ bin/export_cv_nfs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 #Export latest so next time local is used it's more up-to-date
 mkdir /mnt/share
-echo "10.0.0.1:/var/www/html/ks/uteeg /mnt/share	nfs rw,hard,intr,context="system_u:object_r:httpd_sys_rw_content_t:s0" 0 0" >> /etc/fstab
+echo "10.0.0.1:/var/www/html/uteeg /mnt/share   nfs rw,hard,intr,context="system_u:object_r:httpd_sys_rw_content_t:s0" 0 0" >> /etc/fstab
 rmdir /var/lib/pulp/katello-export
 /usr/bin/mount -a
 cd /var/lib/pulp && /usr/bin/ln -s /mnt/share/katello-export .
