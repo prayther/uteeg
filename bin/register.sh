273f5f19da1b11799e7aab0da0ad2b731095ca79
root <root@fedora.prayther.laptop>
2017-07-17 08:31:28 -0400

    register

--- bin/register.sh
+++ bin/register.sh
@@ -5,12 +5,15 @@ source ../etc/install-configure-satellite.cfg
 source ../etc/virt-inst.cfg
 source ../etc/register_cdn.cfg
 
+cd /root && wget --no-clobber http://${SERVER}/passwd
+cd /root && wget --no-clobber http://${SERVER}/rhn-acct
+
 #exec >> log/update.log 2>&1
 
 # Unregister so if your are testing over and over you don't run out of subscriptions and annoy folks.
 # Register.
 /usr/sbin/subscription-manager unregister
-/usr/sbin/subscription-manager --username="${RHN_USERNAME}" --password="${RHN_PASSWD}" register
+/usr/sbin/subscription-manager --username=$(cat rhn-acct) --password=$(cat passwd) register
 /usr/sbin/subscription-manager attach --pool="${RHN_POOL}"     #8a85f9873f77744e013f8944ab87680b
 /usr/sbin/subscription-manager repos '--disable=*'
 /usr/sbin/subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-server-rhscl-7-rpms --enable=rhel-7-server-satellite-6.2-rpms
