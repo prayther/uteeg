afdef57b1952e82ed6c6897432eed83383625b84
root <root@fedora.prayther.laptop>
2017-07-20 15:15:25 -0400

    lifecycles and cv's

--- bin/create_lifecycles.sh
+++ bin/create_lifecycles.sh
@@ -1,3 +1,15 @@
+#!/bin/bash -x
+
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export HOME=/root
+
+cd "${BASH_SOURCE%/*}"
+source ../etc/install-configure-satellite.cfg
+source ../etc/virt-inst.cfg
+source ../etc/register_cdn.cfg
+
+exec >> ../log/create_lifecycles.log 2>&1
+
 #Create 3 lifecycle environment paths
 #    Openshift Apps -> Dev -> Prod
 #    Public_Website -> Dev -> Test -> Prod
