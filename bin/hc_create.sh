9d3201109458a9278f4593dcfbf15b4b2493b60c
Aaron Prayther <prayther@gmail.com>
2017-07-21 16:26:57 -0400

    hc

--- bin/hc_create.sh
+++ bin/hc_create.sh
@@ -19,9 +19,9 @@ exec >> ../log/hc_create.log 2>&1
 # Run after creating Content Views and Lifecycles. It uses those to create the Host Collections.
 # Create a host group for each CCV.
 
-for CV in $(hammer --csv content-view list --organization redhat | grep CCV | awk -F"," '{print$2}' | sed 's/^[^_]*_//g');do
-  for LE in $(hammer --csv lifecycle-environment list --organization redhat | grep -v "Library" | grep -v "Name" | awk -F"," '{print $2}');do
-    hammer host-collection create --name="HC_${LE}_${CV}" --organization=redhat
+for CV in $(hammer --csv content-view list --organization ${ORG} | sort -n | grep CCV | awk -F"," '{print$2}' | sed 's/^[^_]*_//g');do
+  for LE in $(hammer --csv lifecycle-environment list --organization ${ORG} | sort -n | grep -v "Library" | grep -v "ID" | awk -F"," '{print $2}');do
+    hammer host-collection create --name="HC_${LE}_${CV}" --organization=${ORG}
   done
 done
 
