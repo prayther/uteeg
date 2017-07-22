3489eac3754aeca7fb13d35ed7fb21e2703e0933
Aaron Prayther <prayther@gmail.com>
2017-07-22 10:34:28 -0400

    more descriptive name for hg's

--- bin/create_hostgroup.sh
+++ bin/create_hostgroup.sh
@@ -44,7 +44,7 @@ for LOC in ${LOC_var};do
     for CCV in ${CCV_var};do
       for LE in ${LE_var};do
 	for NET in ${NET_var};do
-          hammer hostgroup create --architecture="x86_64" --organization "${ORG}" --locations "${LOC}" --lifecycle-environment ${LE} --content-view CCV_${CCV} --content-source-id 1 --domain="${DOMAIN}" --medium-id="${MEDID}" --name="HG_${LE}_${CCV}_${ORG}_${LOC}" --subnet="${NET}" --partition-table-id="${PARTID}" --operatingsystem-id="${OSID}"
+          hammer hostgroup create --architecture="x86_64" --organization "${ORG}" --locations "${LOC}" --lifecycle-environment ${LE} --content-view CCV_${CCV} --content-source-id 1 --domain="${DOMAIN}" --medium-id="${MEDID}" --name="HG_${LE}_CCV_${CCV}_ORG_${ORG}_LOC_${LOC}" --subnet="${NET}" --partition-table-id="${PARTID}" --operatingsystem-id="${OSID}"
 	  #hammer organization add-hostgroup --name ${ORG} --hostgroup "HG_${LE}_${CCV}"vascript:void(0)
         done
       done
