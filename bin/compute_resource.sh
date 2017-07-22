5beebec966d434de14200cdbab871cd909c197e0
Aaron Prayther <prayther@gmail.com>
2017-07-22 17:27:46 -0400

    compute resource

--- bin/compute_resource.sh
+++ bin/compute_resource.sh
@@ -11,11 +11,15 @@ source ../etc/register_cdn.cfg
 #hammer --cvs location list | awk -F"," '{print $2}'
 #exec >> ../log/compute_resource.log 2>&1
 
-hammer compute-resource create --description 'LibVirt Compute Resource' --locations ${LOC} --name Libvirt_CR --organizations "$ORG" --url 'qemu+tcp://${IP}/system/' --provider libvirt --set-console-password 0
+hammer compute-resource create --description 'LibVirt Compute Resource' --locations ${LOC} --name Libvirt_CR --organizations "$ORG" --url "qemu+ssh://root@${IP}/system/" --provider libvirt --set-console-password 0
 
 firewall-cmd --add-port=5910-5930/tcp
 firewall-cmd --add-port=5910-5930/tcp --permanent
 
-su - foreman -s /bin/bash
-ssh-keygen
-ssh-copy-id root@${GATEWAY}
+# setup for compute resource with libvirt
+#su - foreman -s /bin/bash
+#ssh-keygen
+#ssh-copy-id root@${GATEWAY}
+
+# import crt for libvirt vm console on your workstation/laptop browser
+#http://10.0.0.8/pub/katello-server-ca.crt
