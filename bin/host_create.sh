802bb246e23f1a1f5465eaf8454beec22cf8fa1a
Aaron Prayther <prayther@gmail.com>
2017-07-23 10:23:14 -0400

    ip

--- bin/host_create.sh
+++ bin/host_create.sh
@@ -34,6 +34,6 @@ hammer host create \
 --hostgroup HG_Infra_1_Dev_CCV_RHEL7_Server_ORG_redhat_LOC_laptop \
 --organization redhat \
 --location laptop \
---interface="primary=true,compute_type=network,compute_network=laptoplab" \
+--interface="primary=true,compute_type=network,compute_network=laptoplab,ip=${vmip}" \
 --volume="capacity=10G,format_type=qcow2" \
 --compute-resource Libvirt_CR
