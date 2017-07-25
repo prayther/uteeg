7c43f1c607fad06bc0ac6cc10f317e414f7ad013
Aaron Prayther <prayther@gmail.com>
2017-07-25 18:45:07 -0400

    self register

--- bin/i00010_Satellite_Self_Register.sh
+++ bin/i00010_Satellite_Self_Register.sh
@@ -15,7 +15,7 @@ exec >> ../log/virt_inst.log 2>&1
 /usr/sbin/subscription-manager unregister
 rpm -Uvh /var/www/html/pub/katello-ca-consumer-latest.noarch.rpm
 #/usr/sbin/subscription-manager --username=admin --password=password register --activationkey=
-/usr/sbin/subscription-manager --username=admin --password=password register
+/usr/sbin/subscription-manager --username=admin --password=password register --environment Library
 #/usr/sbin/subscription-manager attach --pool="${RHN_POOL}"     #8a85f9873f77744e013f8944ab87680b
 #/usr/sbin/subscription-manager repos '--disable=*'
 #/usr/sbin/subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-server-rhscl-7-rpms --enable=rhel-7-server-satellite-6.2-rpms
