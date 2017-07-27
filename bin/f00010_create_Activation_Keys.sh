94d837118988d8242ee56a3e221d231997ab727b
Aaron Prayther <prayther@gmail.com>
2017-07-27 12:39:06 -0400

    cleanup

--- bin/f00010_create_Activation_Keys.sh
+++ bin/f00010_create_Activation_Keys.sh
@@ -16,9 +16,12 @@ source ../etc/virt-inst.cfg
 # 4 Layered for ceph, CFME, Satellite that would already have a rhel sub. You would have to isoliate to esxi hosts that do not have rhel subs, other wise you would use a rhel sub and a ceph sub for rhel
 
 # What I did... for now.
-# Add a AK for each CCV. Seems like a good idea.
-for CCV in "${CCV_var}";do
-  for LE in "${LE_var}";do
+# Add a AK for each CCV in each lifecycle. Seems like a good idea.
+LIFECYCLE=$(hammer --csv lifecycle-environment list --organization="${ORG}" | sort -n | awk -F"," '{print $1}' | grep -iv ID | grep -v Library)
+COMPOSITECONTENTVIEW=$(hammer --csv content-view list --organization="${ORG}" | grep -v "Content View ID,Name,Label,Composite,Repository IDs" | grep true | awk -F"," '{print $2}')
+
+for CCV in "${LIFECYCLE}";do
+  for LE in $(echo "${COMPOSITECONTENTVIEW}");do
     hammer activation-key create --name="AK_${LE}_${CCV}" --organization="${ORG}" --lifecycle-environment="${LE}" --content-view="CCV_${CCV}"
     hammer activation-key update --release-version="7Server" --name="AK_${LE}_${CCV}" --organization="${ORG}"
     hammer activation-key add-host-collection --name="AK_${LE}_${CCV}" --organization="${ORG}" --host-collection=HC_"${LE}"_"${CCV}"
