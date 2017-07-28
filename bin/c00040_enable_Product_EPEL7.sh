addbe01080f49a071e03136bd3e6bf32673564c3
Aaron Prayther <prayther@gmail.com>
2017-07-28 09:46:29 -0400

    typo

--- bin/c00040_enable_Product_EPEL7.sh
+++ bin/c00040_enable_Product_EPEL7.sh
@@ -12,14 +12,14 @@ source ../etc/virt-inst.cfg
 # Setup EPEL
 hammer product create --organization ${ORG} --name=RHEL7_EPEL7
 
-curl -f http://"${GATEWAY}/ks/katello-export/redhat-Default_Organization_View-v1.0/redhat/Library/custom/" && hammer repository create --name=RHEL7_EPEL --organization=${ORG} --product=RHEL7_EPEL --content-type='yum' --publish-via-http=true --url=http://${GATEWAY}/ks/katello-export/redhat-Default_Organization_View-v1.0/redhat/Library/custom/EPEL7/EPEL_7_-_x86_64/
+curl -f http://"${GATEWAY}/ks/katello-export/redhat-Default_Organization_View-v1.0/redhat/Library/custom/" && hammer repository create --name=RHEL7_EPEL --organization=${ORG} --product=RHEL7_EPEL --content-type='yum' --publish-via-http=true --url=http://${GATEWAY}/ks/katello-export/redhat-Default_Organization_View-v1.0/redhat/Library/custom/
 # Then we can sync all repositories that we've enable
 for i in $(hammer --csv repository list --organization=${ORG} | grep -i "RHEL7_EPEL" | awk -F, {'print $1'} | grep -vi '^ID')
   do hammer repository synchronize --id ${i} --organization=${ORG}
 done
 
 # Put pulic mirror back to sync latest
-hammer repository update --url ${URL_EPEL} --organization "${ORG}" --product EPEL7
+hammer repository update --url ${URL_EPEL} --organization "${ORG}" --product="RHEL7_EPEL7"
 for i in $(hammer --csv repository list --organization=${ORG} | grep -i "RHEL7_EPEL" | awk -F, {'print $1'} | grep -vi '^ID')
   do hammer repository synchronize --id ${i} --organization=${ORG}
 done
