72fb943b68b83bc24fe0fcc99e94652d3d1337d3
Aaron Prayther <prayther@gmail.com>
2017-07-29 10:51:50 -0400

    type

--- bin/a00010_satellite-install.sh
+++ bin/a00010_satellite-install.sh
@@ -3,11 +3,11 @@
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 export HOME=/root
 cd "${BASH_SOURCE%/*}"
-LogFile="log/virt-inst.log"
+LogFile="../log/virt-inst.log"
 LOG_() { while IFS='' read -r line; do echo "$(date)-${0} $line" >> "${LogFile}"; done; }
 exec 2> >(LOG_)
 
-source etc/virt-inst.cfg
+source ../etc/virt-inst.cfg
 
 cd /root && wget --no-clobber http://${SERVER}/ks/iso/${SATELLITE_ISO}
 cd /root && wget --no-clobber http://${SERVER}/ks/iso/${RHEL_ISO}
