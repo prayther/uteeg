fb4c14c72f22e23bd35461e54855f278d66540fa
Aaron Prayther <prayther@gmail.com>
2017-07-30 12:06:23 -0400

    doit

--- bin/b00010_satellite-update.sh
+++ bin/b00010_satellite-update.sh
@@ -1,19 +1,51 @@
 #!/bin/bash -x
 
+#https://github.com/prayther/uteeg
+#http://www.opensourcerers.org/installing-and-configuring-red-hat-satellite-6-via-shell-script/
+# mschreie@redhat.com
+# setting up  a satellite for demo purposes
+# mainly following Adrian Bredshaws awsome book: http://gsw-hammer.documentation.rocks/
+
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 export HOME=/root
 cd "${BASH_SOURCE%/*}"
-LogFile="../log/virt-inst.log"
-LOG_() { while IFS='' read -r line; do echo "$(date)-${0} $line" >> "${LogFile}"; done; }
-exec 2> >(LOG_)
 
+logfile="../log/$(basename $0 .sh).log"
+donefile="../log/$(basename $0 .sh).done"
+touch $logfile
+touch $donefile
+
+exec > >(tee -a "$logfile") 2>&1
+
+echo "###INFO: Starting $0"
+echo "###INFO: $(date)"
+
+# read configuration (needs to be adopted!)
+#. ./satenv.sh
 source ../etc/virt-inst.cfg
 
+
+doit() {
+        echo "INFO: doit: $@" >&2
+        cmd2grep=$(echo "$*" | sed -e 's/\\//' | tr '\n' ' ')
+        grep -q "$cmd2grep" $donefile
+        if [ $? -eq 0 ] ; then
+                echo "INFO: doit: found cmd in donefile - skipping" >&2
+        else
+                "$@" 2>&1 || {
+                        echo "ERROR: cmd was unsuccessfull RC: $? - bailing out" >&2
+                        exit 1
+                }
+                echo "$cmd2grep" >> $donefile
+                echo "INFO: doit: cmd finished successfull" >&2
+        fi
+}
+
 # This seems to work as a 'safety net' and upgrades after installing from media
-satellite-installer --scenario satellite --upgrade
+doit satellite-installer --scenario satellite --upgrade
 
 # Notice adding of tftp for provisioning from capsules and the removal of Puppet
-/usr/sbin/satellite-installer --scenario satellite \
+doit /usr/sbin/satellite-installer --scenario satellite \
 --foreman-initial-organization "${ORG}" \
 --foreman-initial-location "${LOC}" \
 --foreman-admin-username admin \
@@ -23,8 +55,8 @@ satellite-installer --scenario satellite --upgrade
 --capsule-puppet false
 
 #Upload our manifest.zip (created in RH Portal)
-cd /root && wget --no-clobber http://${SERVER}/ks/manifest/manifest.zip
-hammer subscription upload --file /root/manifest.zip  --organization=${ORG}
+doit wget -P /root/ --no-clobber http://${SERVER}/ks/manifest/manifest.zip
+doit hammer subscription upload --file /root/manifest.zip  --organization=${ORG}
 
 # timeout for testing.
-hammer settings set --name idle_timeout --value 99999999
+doit hammer settings set --name idle_timeout --value 99999999
