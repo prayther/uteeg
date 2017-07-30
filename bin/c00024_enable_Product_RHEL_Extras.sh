8c190e6bbb0a52b74c1b8f04d827ca995f467211
Aaron Prayther <prayther@gmail.com>
2017-07-30 14:01:09 -0400

    remove doit from curl tst and cdn url change

--- bin/c00024_enable_Product_RHEL_Extras.sh
+++ bin/c00024_enable_Product_RHEL_Extras.sh
@@ -42,7 +42,7 @@ doit() {
 }
 
 ## RHEL 7 basic repos from local for speed, then again later, changing to internet sources to get updated.
-doit curl -f http://"${GATEWAY}"/ks/katello-export && hammer organization update --name ${ORG} --redhat-repository-url ${URL}/katello-export/redhat-Default_Organization_View-v1.0/redhat/Library/
+curl -f http://"${GATEWAY}"/ks/katello-export && hammer organization update --name ${ORG} --redhat-repository-url ${URL}/katello-export/redhat-Default_Organization_View-v1.0/redhat/Library/
 #hammer repository-set enable --organization "${ORG}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7.3' --name 'Red Hat Enterprise Linux 7 Server (Kickstart)'
 #hammer repository-set enable --organization "${ORG}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server (RPMs)'
 #hammer repository-set enable --organization "${ORG}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7.3' --name 'Red Hat Enterprise Linux 7 Server RPMs x86_64 7.3'
