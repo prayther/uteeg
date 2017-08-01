444163941a18da641c237c5b1db4e3feda8613e4
Aaron Prayther <prayther@gmail.com>
2017-08-01 08:08:15 -0400

    taking another shot at CCV

--- bin/u02000_create_Host_Collections.sh
+++ bin/u02000_create_Host_Collections.sh
@@ -44,7 +44,7 @@ doit() {
 # Run after creating Content Views and Lifecycles. It use those to create the Host Collections.
 # Create a host group for each CCV.
 
-cv_lifecycle () { for CV in $(hammer --csv content-view list --organization ${ORG} | sort -n | grep CV | awk -F"," '{print$2}');do
+cv_lifecycle () { for CV in $(hammer --csv content-view list --organization ${ORG} | sort -n | grep CCV | awk -F"," '{print$2}');do
   for LE in $(hammer --csv lifecycle-environment list --organization ${ORG} | sort -n | awk -F"," '{print $2}' | grep -v "Library" | grep -v "Name");do
     hammer host-collection create --name="HC_${LE}_${CV}" --organization=${ORG}
   done
