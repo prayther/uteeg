6dadb5b353a5e34309586929b7dbf7c6498d25b4
Aaron Prayther <prayther@gmail.com>
2017-08-05 17:34:24 -0400

    implementing naming scheme with uniq numeric code for eash product content

--- bin/r01000_enable_Product_RHEL.sh
+++ bin/r01000_enable_Product_RHEL.sh
@@ -42,12 +42,12 @@ doit() {
 }
 
 # Enable RHEl core
-doit hammer repository-set enable --organization "${ORG}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7.4' --name 'Red Hat Enterprise Linux 7 Server (Kickstart)'
-doit hammer repository-set enable --organization "${ORG}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server (RPMs)'
-doit hammer repository-set enable --organization "$ORG" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --name 'Red Hat Satellite Tools 6.2 (for RHEL 7 Server) (RPMs)'
+doit hammer repository-set enable --organization="${ORG}" --new-name="${RHELKS}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7.4' --name 'Red Hat Enterprise Linux 7 Server (Kickstart)'
+doit hammer repository-set enable --organization="${ORG}" --new-name="${RHEL}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server (RPMs)'
+doit hammer repository-set enable --organization="$ORG" --new-name="${TOOLS}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --name 'Red Hat Satellite Tools 6.2 (for RHEL 7 Server) (RPMs)'
 
 # Then we can sync all repositories that we've enable
-repo_sync () { for i in $(hammer --csv repository list --organization=${ORG} | grep -i "${PRODUCT_VER}" | awk -F, {'print $1'} | grep -vi '^ID')
+repo_sync () { for i in $(hammer --csv repository list --organization=${ORG} | grep -E "2455|2456|4831" | awk -F, {'print $1'})
   do hammer repository synchronize --id ${i} --organization=${ORG}
 done
 }
@@ -63,33 +63,31 @@ echo "###INFO: $(date)"
 #13 | Red Hat Enterprise Linux 7 Server RPMs x86_64 7.3         | Red Hat Enterprise Linux Server | yum          | https://cdn.redhat.com/content/dist/rhel/server/7/7.3/x86_64/os
 #1  | Red Hat Enterprise Linux 7 Server Kickstart x86_64 7.3    | Red Hat Enterprise Linux Server | yum          | https://cdn.redhat.com/content/dist/rhel/server/7/7.3/x86_64/kickstart
 #hammer repository-set list --organization "${ORG}" --product 'Red Hat Enterprise Linux Server' | grep "7 Server" | grep -vi source | grep -vi iso | grep -vi debug | less
-#4380 | yum       | Red Hat Satellite Tools 6 Beta (for RHEL 7 Server) (RPMs)
-#4922 | yum       | Red Hat Ceph Storage Tools 2 for Red Hat Enterprise Linux 7 Server (RPMs)
-#5091 | yum       | Red Hat Insights Client 1 (for RHEL 7 Server) (RPMs)
-#5726 | yum       | Red Hat OpenStack Platform 11 Tools for RHEL 7 Server (RPMs)
-#2460 | yum       | Red Hat Enterprise Linux 7 Server - Fastrack (RPMs)
-#2480 | yum       | Red Hat Enterprise Linux 7 Server - Supplementary Beta (RPMs)
-#4725 | yum       | Red Hat OpenStack Platform 8 Tools for RHEL 7 Server (RPMs)
-#4831 | yum       | Red Hat Satellite Tools 6.2 (for RHEL 7 Server) (RPMs)
-#2484 | yum       | Red Hat Enterprise Linux 7 Server Beta (RPMs)
-#4539 | yum       | Red Hat OpenStack Tools 7.0 for Red Hat Enterprise Linux 7 Server (RPMs)
-#2463 | yum       | Red Hat Enterprise Linux 7 Server - Optional (RPMs)
-#2466 | yum       | Red Hat Enterprise Linux 7 Server - Optional Beta (RPMs)
-#2455 | kickstart | Red Hat Enterprise Linux 7 Server (Kickstart)
-#3030 | yum       | Red Hat Enterprise Linux 7 Server - Extras (RPMs)
-#6011 | yum       | Red Hat Enterprise Linux 7 Server - Extras Beta (RPMs)
-#4188 | yum       | Red Hat Satellite Tools 6.1 (for RHEL 7 Server) (RPMs)
-#5362 | yum       | Red Hat Satellite Tools 6.3 (for RHEL 7 Server) (RPMs)
-#5916 | yum       | Red Hat OpenStack Platform 12 Tools for RHEL 7 Server (RPMs)
-#3950 | kickstart | Red Hat Enterprise Linux 7 Server Beta (Kickstart)
-#5064 | yum       | Red Hat OpenStack Platform 10 Tools for RHEL 7 Server (RPMs)
-#2472 | yum       | Red Hat Enterprise Linux 7 Server - RH Common (RPMs)
-#2469 | yum       | Red Hat Enterprise Linux 7 Server - Optional Fastrack (RPMs)
-#5996 | yum       | Red Hat Satellite Maintenance 6 (for RHEL 7 Server) (RPMs)
-#5048 | yum       | Red Hat OpenStack Platform 9 Tools for RHEL 7 Server (RPMs)
-#2456 | yum       | Red Hat Enterprise Linux 7 Server (RPMs)
-#3327 | yum       | RHN Tools for Red Hat Enterprise Linux 7 Server (RPMs)
-#3512 | yum       | Red Hat Enterprise Linux 7 Server - RH Common Beta (RPMs)
-#2476 | yum       | Red Hat Enterprise Linux 7 Server - Supplementary (RPMs)
-#4455 | yum       | Red Hat Ceph Storage Tools 1.3 for Red Hat Enterprise Linux 7 Server (RPMs)
-#3331 | yum       | RHN Tools for Red Hat Enterprise Linux 7 Server Beta (RPMs)
+
+#4743 | Red Hat Satellite 6.2 (for RHEL 7 Server) (RPMs)
+#4751 | Red Hat Satellite Capsule 6.2 (for RHEL 7 Server) (RPMs)
+#2808 | Red Hat Software Collections RPMs for Red Hat Enterprise Linux 7 Server
+
+#2455 | Red Hat Enterprise Linux 7 Server (Kickstart)
+#2456 | Red Hat Enterprise Linux 7 Server (RPMs)
+#2460 | Red Hat Enterprise Linux 7 Server - Fastrack (RPMs)
+#2463 | Red Hat Enterprise Linux 7 Server - Optional (RPMs)
+#2469 | Red Hat Enterprise Linux 7 Server - Optional Fastrack (RPMs)
+#2472 | Red Hat Enterprise Linux 7 Server - RH Common (RPMs)
+#2476 | Red Hat Enterprise Linux 7 Server - Supplementary (RPMs)
+#3030 | Red Hat Enterprise Linux 7 Server - Extras (RPMs)
+#3327 | RHN Tools for Red Hat Enterprise Linux 7 Server (RPMs)
+#4188 | Red Hat Satellite Tools 6.1 (for RHEL 7 Server) (RPMs)
+#4455 | Red Hat Ceph Storage Tools 1.3 for Red Hat Enterprise Linux 7 Server (RPMs)
+#4539 | Red Hat OpenStack Tools 7.0 for Red Hat Enterprise Linux 7 Server (RPMs)
+#4725 | Red Hat OpenStack Platform 8 Tools for RHEL 7 Server (RPMs)
+#4831 | Red Hat Satellite Tools 6.2 (for RHEL 7 Server) (RPMs)
+#4922 | Red Hat Ceph Storage Tools 2 for Red Hat Enterprise Linux 7 Server (RPMs)
+#5048 | Red Hat OpenStack Platform 9 Tools for RHEL 7 Server (RPMs)
+#5064 | Red Hat OpenStack Platform 10 Tools for RHEL 7 Server (RPMs)
+#5091 | Red Hat Insights Client 1 (for RHEL 7 Server) (RPMs)
+#5362 | Red Hat Satellite Tools 6.3 (for RHEL 7 Server) (RPMs)
+#5726 | Red Hat OpenStack Platform 11 Tools for RHEL 7 Server (RPMs)
+#5916 | Red Hat OpenStack Platform 12 Tools for RHEL 7 Server (RPMs)
+#5996 | Red Hat Satellite Maintenance 6 (for RHEL 7 Server) (RPMs)
+
