db058794e1d80422437b85f13d2c1c48ec3ae67c
Aaron Prayther <prayther@gmail.com>
2017-08-13 16:21:33 -0400

    add git uteeg to ceph user

--- virt-inst.sh
+++ virt-inst.sh
@@ -213,6 +213,7 @@ exec 2> >(LOG_)
 
 #Copy over the main script for configuration
 cd /root && /usr/bin/git clone https://github.com/prayther/uteeg.git
+cd ~/"${CEPH_USER}" && /usr/bin/git clone https://github.com/prayther/uteeg.git
 $ it gets extraneous stuff in there from my laptop.
 > /root/uteeg/log/virt-inst.log
 
