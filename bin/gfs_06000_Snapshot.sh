d3139c57387b7bfc15fd4f83f45c5da90a8113ed
Aaron Prayther <prayther@gmail.com>
2017-09-04 07:28:54 -0400

    moved sleep up above cron_init

--- bin/gfs_06000_Snapshot.sh
+++ bin/gfs_06000_Snapshot.sh
@@ -106,13 +106,13 @@ for i in gfs-admin.prayther.org gfs-node1.prayther.org gfs-node2.prayther.org gf
   do ssh "${i}" "mount -t glusterfs gfs-node2:/distreplvol /mnt/distreplvol"
 done
 
+#getting this error and don't know why. but becomes available. so gonna sleep for a second
+#snap_scheduler: Failed: /var/run/gluster/shared_storage does not exist.
+sleep 15
 for i in gfs-admin.prayther.org gfs-node1.prayther.org gfs-node2.prayther.org gfs-node3.prayther.org
   do ssh "${i}" "snap_scheduler.py init"
 done
 
-#getting this error and don't know why. but becomes available. so gonna sleep for a second
-#snap_scheduler: Failed: /var/run/gluster/shared_storage does not exist.
-sleep 15
 #on one srv enable the snapshot scheduler.
 ssh gfs-admin.prayther.org "snap_scheduler.py enable"
 
