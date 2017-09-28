7f0d2fe01a828b795469391cc618ac5faf8f1e61
root <root@fedora-26.prayther.org>
2017-09-28 09:30:21 -0400

    adding ansible-admin with node1

---
+++ bin/ansible_00100_install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export HOME=/root
+cd "${BASH_SOURCE%/*}"
+
+# bsfl are bash libs used in scripts in uteeg
+ls -d ~/bsfl || git clone https://github.com/SkypLabs/bsfl.git /root/bsfl
+
+# read configuration (needs to be adopted!)
+#source etc/virt-inst.cfg
+source etc/virthost.cfg
+source etc/rhel.cfg
+source ./bsfl/lib/bsfl.sh || exit 1
+DEBUG=no
+LOG_ENABLED="yes"
+SYSLOG_ENABLED="yes"
+
+#runs or not based on hostname; ceph-?? gfs-??? sat-???
+if [[ $(hostname -s | awk -F"-" '{print $1}') -ne "ansible" ]];then
+ echo ""
+ echo "Need to run this on the 'gfs' node"
+ echo ""
+ exit 1
+fi
+
+if [[ $(hostname -s | awk -F"-" '{print $2}') -ne "admin" ]];then
+ echo ""
+ echo "Need to run this on the 'admin' node"
+ echo ""
+ exit 1
+fi
+
+if [[ $(id -u) != "0" ]];then
+        echo "Must run as root"
+        echo
+        exit 1
+fi
+
+yum -y install ansible
+ansible --version
+
+su -c "mkdir -pv ~/lab/inventory" user
+
+su -c "cat << "EOF" > ~/lab/ansible.cfg
+[defaults]
+remote_user = user
+inventory = inventory
+
+[privilege_escalation]
+become = False
+become_method = sudo
+become_user = root
+become_ask_pass = False
+EOF" user
+
+su -c "cat << "EOF" > ~/lab/ansible.cfg
+[intranetweb]
+ansible-node1
+
+[everyone:children]
+intranetweb
+EOF" user
+
+ansible everyone -m command -a 'id'
