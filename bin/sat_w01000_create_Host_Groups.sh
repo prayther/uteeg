aa082edd5fe32193ab5435328b1b020bdcabb6ec
root <root@fedora26.prayther.org>
2017-10-07 10:00:59 -0400

    removing CCV from host groups creation, only using CV for now.

--- bin/sat_w01000_create_Host_Groups.sh
+++ bin/sat_w01000_create_Host_Groups.sh
@@ -45,7 +45,8 @@ doit() {
 # This is setup for 'all' not just dev. so you get errors so turning off 'run once' with doit
 setup_slow_vars () {
                     LE_var=$(hammer --csv lifecycle-environment list --organization="${ORG}" | sort -n | awk -F"," '{print $2}' | grep -iv name | grep -v Library)
-                    CCV_var=$(hammer --csv content-view list --organization="${ORG}" | grep -v "Content View ID,Name,Label,Composite,Repository IDs" | grep true | awk -F"," '{print $2}')
+                    #CCV_var=$(hammer --csv content-view list --organization="${ORG}" | grep -v "Content View ID,Name,Label,Composite,Repository IDs" | grep true | awk -F"," '{print $2}')
+                    CV_var=$(hammer --csv content-view list --organization="${ORG}" | grep -v "Content View ID,Name,Label,Composite,Repository IDs" | grep false | awk -F"," '{print $2}')
                     LOC_var=$(hammer --csv location list | grep -iv id,name | awk -F"," '{print $2}')
                     ORG_var=$(hammer --csv organization list | grep -iv id,name | awk -F"," '{print $2}')
                     NET_var=$(hammer --csv subnet list | grep -vi id,name | awk -F"," '{print $2}')
@@ -60,11 +61,14 @@ setup_slow_vars
 hostgroup_create () {
 for LOC in $(echo "${LOC_var}");do
   for ORG_local in $(echo "${ORG_var}");do
-    for CCV in $(echo "${CCV_var}");do
+    #for CCV in $(echo "${CCV_var}");do
+    for CV in $(echo "${CCV_var}");do
       for LE in $(echo "${LE_var}");do
         for NET in $(echo "${NET_var}");do
-          hammer hostgroup create --root-pass ${PASSWD} --architecture="x86_64" --organization "${ORG_local}" --locations "${LOC}" --lifecycle-environment ${LE} --content-view ${CCV} --content-source-id 1 --domain="${DOMAIN}" --medium-id="${MEDID}" --name="HG_${LE}_${CCV}_ORG_${ORG_local}_LOC_${LOC}" --subnet="${NET}" --partition-table-id="${PARTID}" --operatingsystem-id="${OSID}"
-	  hammer hostgroup set-parameter --hostgroup "HG_${LE}_${CCV}_ORG_${ORG_local}_LOC_${LOC}" --value AK_${LE}_${CCV} --name "kt_activation_keys"
+          #hammer hostgroup create --root-pass ${PASSWD} --architecture="x86_64" --organization "${ORG_local}" --locations "${LOC}" --lifecycle-environment ${LE} --content-view ${CCV} --content-source-id 1 --domain="${DOMAIN}" --medium-id="${MEDID}" --name="HG_${LE}_${CCV}_ORG_${ORG_local}_LOC_${LOC}" --subnet="${NET}" --partition-table-id="${PARTID}" --operatingsystem-id="${OSID}"
+          hammer hostgroup create --root-pass ${PASSWD} --architecture="x86_64" --organization "${ORG_local}" --locations "${LOC}" --lifecycle-environment ${LE} --content-view ${CV} --content-source-id 1 --domain="${DOMAIN}" --medium-id="${MEDID}" --name="HG_${LE}_${CV}_ORG_${ORG_local}_LOC_${LOC}" --subnet="${NET}" --partition-table-id="${PARTID}" --operatingsystem-id="${OSID}"
+	  #hammer hostgroup set-parameter --hostgroup "HG_${LE}_${CCV}_ORG_${ORG_local}_LOC_${LOC}" --value AK_${LE}_${CCV} --name "kt_activation_keys"
+	  hammer hostgroup set-parameter --hostgroup "HG_${LE}_${CV}_ORG_${ORG_local}_LOC_${LOC}" --value AK_${LE}_${CV} --name "kt_activation_keys"
         done
       done
     done
