bdda199c76d029e80a3ef174cf67fd39935154f1
root <root@fedora27.prayther.org>
2018-01-19 09:55:00 -0500

    added basics for foreman-rake to clean up tasks

---
+++ bin/daily_cleanup_tasks.sh
@@ -0,0 +1,59 @@
+!/bin/bash -x
+
+#https://github.com/prayther/uteeg
+#http://www.opensourcerers.org/installing-and-configuring-red-hat-satellite-6-via-shell-script/
+# mschreie@redhat.com
+# setting up  a satellite for demo purposes
+# mainly following Adrian Bredshaws awsome book: http://gsw-hammer.documentation.rocks/
+
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export HOME=/root
+cd "${BASH_SOURCE%/*}"
+
+logfile="../log/$(basename $0 .sh).log"
+donefile="../log/$(basename $0 .sh).done"
+touch $logfile
+touch $donefile
+
+exec > >(tee -a "$logfile") 2>&1
+
+echo "###INFO: Starting $0"
+echo "###INFO: $(date)"
+
+# read configuration (needs to be adopted!)
+#. ./satenv.sh
+source ../etc/virt-inst.cfg
+
+
+doit() {
+        echo "INFO: doit: $@" >&2
+        cmd2grep=$(echo "$*" | sed -e 's/\\//' | tr '\n' ' ')
+        grep -q "$cmd2grep" $donefile
+        if [ $? -eq 0 ] ; then
+                echo "INFO: doit: found cmd in donefile - skipping" >&2
+        else
+                "$@" 2>&1 || {
+                        echo "ERROR: cmd was unsuccessfull RC: $? - bailing out" >&2
+                        exit 1
+                }
+                echo "$cmd2grep" >> $donefile
+                echo "INFO: doit: cmd finished successfull" >&2
+        fi
+}
+
+# these are the basic steps for cleaning up tasks that won't go away in Task GUI.
+# mix and match the parameters base on the return from "select * FROM foreman_tasks_tasks where state != 'stopped';"
+# then you can clean up whatever task needed.
+
+#su - postgres
+#psql foreman
+#foreman=# select * FROM foreman_tasks_tasks where state != 'stopped';
+
+#foreman-rake console
+##irb(main):001:0> ForemanTasks::Task.where(:state => :paused).where(:label => "Actions::Katello::Repository::Sync").destroy_all
+#ForemanTasks::Task.where(:label => "Actions::Katello::ContentView::Publish").destroy_all
+#katello-service restart
+#foreman-rake katello:reindex
+
+echo "###INFO: Finished $0"
+echo "###INFO: $(date)"
