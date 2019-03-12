58ac0eae511fbbe67e341b5be3edacc428c8c1a6
root <root@sat64.example.org>
2019-03-12 09:36:25 -0400

    updating for sat64

--- bin/sat_r01000_enable_Product_RHEL.sh
+++ bin/sat_r01000_enable_Product_RHEL.sh
@@ -48,7 +48,7 @@ doit() {
 #doit hammer repository-set enable --organization="$ORG" --new-name="${TOOLS}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --name 'Red Hat Satellite Tools 6.2 (for RHEL 7 Server) (RPMs)'
 doit hammer repository-set enable --organization="${ORG}" --name="${RHELKS}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7.5' --name 'Red Hat Enterprise Linux 7 Server (Kickstart)'
 doit hammer repository-set enable --organization="${ORG}" --name="${RHEL}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server (RPMs)'
-doit hammer repository-set enable --organization="$ORG" --name="${TOOLS}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --name 'Red Hat Satellite Tools 6.3 (for RHEL 7 Server) (RPMs)'
+doit hammer repository-set enable --organization="$ORG" --name="${TOOLS}" --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --name 'Red Hat Satellite Tools 6.4 (for RHEL 7 Server) (RPMs)'
 
 # Then we can sync all repositories that we've enable
 #repo_sync () { for i in $(hammer --csv repository list --organization=${ORG} | grep -i "${PRODUCT_VER}" | awk -F, {'print $1'} | grep -vi '^ID')
